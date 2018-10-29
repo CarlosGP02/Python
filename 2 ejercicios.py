e16f1f15adacdd26bd7908d04452b1aa1be82acd
CarlosGP02 <44443355+CarlosGP02@users.noreply.github.com>
2018-10-29 19:25:03 +0100

    Update 2 ejercicios.py

--- 2 ejercicios.py
+++ 2 ejercicios.py
@@ -1,3 +1,4 @@
+1
 edad= int(input("Cuantos años tienes?"))
 if (edad>=65) or (edad<5):
     print("Con",edad,"años;te sale gratis")
@@ -7,7 +8,7 @@ else:
     
     
     
-    
+2 
 sexo=input("Que sexo eres?")
 pelo=input("De que color es tu pelo")
 edad=int(input("Cuantos años tienes?"))
@@ -19,3 +20,11 @@ if (edad<=65)and(sexo=="M")and(pelo=="R"):
     print("Con",edad,"siendo",sexo,"y con el pelo",pelo,"te sale gratis")
 else:
     print("Con",edad,"siendo",sexo,"y con el pelo",pelo,"pagas 0,5")
+    
+    
+    
+    
+    3
+    edad= int(input("Cuantos años tienes?"))
+if (edad== 17 )or (edad<=23) or (edad>=18):
+    print("Con",edad,"años;tienes que ir a la sesión jóvenes")
